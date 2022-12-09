8cb802ca49f0512d762ea146213df2022aaac7f7
razakhan2526 <razagulkhan62@gmail.com>
2022-12-09 20:31:41 +0500

    Update Faqs Section & Others

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// chnage navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100)
+});
+
+// Show/hide faq answer
+const faqs = document.querySelectorAll('.faq')
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        }else{
+            icon.className = "uil uil-plus";
+        }
+    });
+});
